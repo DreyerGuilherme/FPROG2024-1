64222442fe488ec38e5505993db2300b27daa67b
Guilherme Meine Dreyer <162520007+DreyerGuilherme@users.noreply.github.com>
2024-04-28 02:16:53 -0300

    lista4

--- Lista4/ex7.py
+++ Lista4/ex7.py
@@ -1,1 +1,11 @@
-#não entendi o que é pra fazer
+nomes = []
+
+for i in range(5):
+    nome = input(f"Informe o nome {i + 1}: ")
+    nomes.append(nome)
+
+nomesOrdenados = sorted(nomes)
+
+primeiroNome = nomesOrdenados[0]
+
+print(f"O primeiro nome em ordem alfabética é: {primeiroNome}.")
